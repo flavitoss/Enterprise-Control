4a6e6a995c2818ccac5275c638d0af4eb796ecec
flavio <flavio.silvapinkman@gmail.com>
2024-04-24 10:25:18 -0300

    created Seller class

--- EnterpriseControl/src/application/Program.java
+++ EnterpriseControl/src/application/Program.java
@@ -1,14 +1,18 @@
 package application;
 
 import model.entities.Department;
+import model.entities.Seller;
+
+import java.util.Date;
 
 public class Program {
 
     public static void main(String[] args) {
 
         Department dp = new Department(1,"Books");
+        Seller seller = new Seller(1,"bob","bob@gmail.com",new Date(), 3000.0, dp);
 
-        System.out.println(dp);
+        System.out.println(seller);
 
     }
 }
