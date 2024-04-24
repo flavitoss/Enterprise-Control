700ae10d2e34609f2566dd3ad4674704673c6d68
flavio <flavio.silvapinkman@gmail.com>
2024-04-24 10:38:52 -0300

    created SellerDao implementation and Dao factory class

--- EnterpriseControl/src/application/Program.java
+++ EnterpriseControl/src/application/Program.java
@@ -1,5 +1,7 @@
 package application;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -12,6 +14,7 @@ public class Program {
         Department dp = new Department(1,"Books");
         Seller seller = new Seller(1,"bob","bob@gmail.com",new Date(), 3000.0, dp);
 
+        SellerDao sellerDao = DaoFactory.createSellerDao();
         System.out.println(seller);
 
     }
