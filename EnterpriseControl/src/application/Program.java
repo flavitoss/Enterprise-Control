d70069eeffcef9cfe601e278731f21e08880d13d
flavio <flavio.silvapinkman@gmail.com>
2024-04-25 11:44:22 -0300

    implemented update function

--- EnterpriseControl/src/application/Program.java
+++ EnterpriseControl/src/application/Program.java
@@ -16,11 +16,12 @@ public class Program {
         SellerDao sellerDao = DaoFactory.createSellerDao();
 
         System.out.println("=== TEST 1: seller findById ===");
-        Seller seller = sellerDao.findById(3);
 
+        Seller seller = sellerDao.findById(3);
         System.out.println(seller);
 
         System.out.println("\n=== TEST 2: seller findByDepartment ===");
+
         Department department = new Department(2,null);
         List<Seller> list = sellerDao.findByDepartment(department);
         list.forEach(System.out::println);
@@ -31,9 +32,20 @@ public class Program {
         list.forEach(System.out::println);
 
         System.out.println("\n=== TEST 4: seller insert ===");
+
         Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
         sellerDao.insert(newSeller);
         System.out.println("Inserted! new id: " + newSeller.getId());
+
+        System.out.println("\n=== TEST 5: seller update ===");
+        
+        seller = sellerDao.findById(1);
+        seller.setName("Martha Wayne");
+        sellerDao.update(seller);
+        System.out.println("Updated! id: " + seller.getId());
+
+
+
 
 
     }
